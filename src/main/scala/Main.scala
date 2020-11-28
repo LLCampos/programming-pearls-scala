279aeb6ddc80dd1f0f5ef5202533fc7d3c166f26
LLCampos <luis.filipe.lcampos@gmail.com>
2020-11-28 12:05:49 +0000

    add general main

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,7 @@
+import part1.column1.CrackingTheOyster
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    CrackingTheOyster.sortFile("src/test/scala/resources/part1/column1/listSize5000.txt")
+  }
+}
