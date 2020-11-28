137792c80301f3a8135f086d63d3c5f21410180b
LLCampos <luis.filipe.lcampos@gmail.com>
2020-11-28 12:06:05 +0000

    improve CrackingTheOysterBenchmarkTest

--- src/test/scala/part1/column1/CrackingTheOysterBenchmarkTest.scala
+++ src/test/scala/part1/column1/CrackingTheOysterBenchmarkTest.scala
@@ -1,14 +1,17 @@
 package part1.column1
 
 import org.scalameter.api._
+import org.scalameter.picklers.noPickler.instance
 
 class CrackingTheOysterBenchmarkTest extends Bench.LocalTime {
-  private val nullGen = Gen.unit("null")
+  //override def measurer = new Measurer.MemoryFootprint
+
+  private val fileSizeGen = Gen.enumeration("fileSize")(1000, 2500, 5000)
 
   performance of "CrackingTheOyster" in {
     measure method "sortFile" in {
-      using(nullGen) in { _ =>
-        CrackingTheOyster.sortFile("src/test/scala/resources/part1/column1/listSize10000.txt")
+      using(fileSizeGen) in { size =>
+        CrackingTheOyster.sortFile(s"src/test/scala/resources/part1/column1/listSize$size.txt")
       }
     }
   }
