18e552bf64fea25fc5648262934269d78865f9d3
LLCampos <luis.filipe.lcampos@gmail.com>
2020-12-04 11:52:44 +0000

    refactor

--- src/test/scala/part1/column1/CrackingTheOysterTest.scala
+++ src/test/scala/part1/column1/CrackingTheOysterTest.scala
@@ -22,22 +22,22 @@ class CrackingTheOysterTest extends Specification with BeforeEach {
       Fragments.foreach(Seq(MergeSort, ScalaDefaultSort, ScalaMergeSort)) { algorithm =>
         s"${algorithm.getClass.getName}" should {
           "be able to sort small list with odd size" in {
-            val result = CrackingTheOyster.sortFile("src/test/scala/resources/part1/column1/listSize3.txt", algorithm)
+            val result = CrackingTheOyster.sortFile(s"$PathToTestFiles/listSize3.txt", algorithm)
             result must be equalTo List(1111111, 2222222, 3333333)
           }
 
           "be able to sort small list with odd size" in {
-            val result = CrackingTheOyster.sortFile("src/test/scala/resources/part1/column1/listSize4.txt", algorithm)
+            val result = CrackingTheOyster.sortFile(s"$PathToTestFiles/listSize4.txt", algorithm)
             result must be equalTo List(1111111, 2222222, 3333333, 4444444)
           }
 
           "return unmodified list of only one element" in {
-            val result = CrackingTheOyster.sortFile("src/test/scala/resources/part1/column1/listSize1.txt", algorithm)
+            val result = CrackingTheOyster.sortFile(s"$PathToTestFiles/listSize1.txt", algorithm)
             result must be equalTo List(2222222)
           }
 
           "return empty list on empty life" in {
-            val result = CrackingTheOyster.sortFile("src/test/scala/resources/part1/column1/listSize0.txt", algorithm)
+            val result = CrackingTheOyster.sortFile(s"$PathToTestFiles/listSize0.txt", algorithm)
             result must be equalTo List()
           }
         }
@@ -54,10 +54,7 @@ class CrackingTheOysterTest extends Specification with BeforeEach {
               algorithm,
               2)
 
-            val file = Source.fromFile(ResultFileName)
-            val result = file.getLines().toList.map(_.toInt)
-            file.close()
-            result must be equalTo List(1111111, 2222222, 3333333)
+            getResultFileLines must be equalTo List(1111111, 2222222, 3333333)
           }
 
           "be able to sort small list with even size" in {
@@ -67,10 +64,7 @@ class CrackingTheOysterTest extends Specification with BeforeEach {
               algorithm,
               2)
 
-            val file = Source.fromFile(ResultFileName)
-            val result = file.getLines().toList.map(_.toInt)
-            file.close()
-            result must be equalTo List(1111111, 2222222, 3333333, 4444444)
+            getResultFileLines must be equalTo List(1111111, 2222222, 3333333, 4444444)
           }
 
           "return unmodified list of only one element" in {
@@ -80,10 +74,7 @@ class CrackingTheOysterTest extends Specification with BeforeEach {
               algorithm,
               2)
 
-            val file = Source.fromFile(ResultFileName)
-            val result = file.getLines().toList.map(_.toInt)
-            file.close()
-            result must be equalTo List(2222222)
+            getResultFileLines must be equalTo List(2222222)
           }
 
           "return empty list on empty life" in {
@@ -93,13 +84,17 @@ class CrackingTheOysterTest extends Specification with BeforeEach {
               algorithm,
               2)
 
-            val file = Source.fromFile(ResultFileName)
-            val result = file.getLines().toList.map(_.toInt)
-            file.close()
-            result must be equalTo List()
+            getResultFileLines must be equalTo List()
           }
         }
       }
     }
+  }
+
+  def getResultFileLines: Seq[Int] = {
+    val file = Source.fromFile(ResultFileName)
+    val result = file.getLines().toList.map(_.toInt)
+    file.close()
+    result
   }
 }
