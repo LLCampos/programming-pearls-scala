b2bbf30276ab98357c24f7d3f56cec45fdd865a1
LLCampos <luis.filipe.lcampos@gmail.com>
2020-12-04 12:14:16 +0000

    benchmark external merge sort

--- src/test/scala/part1/column1/CrackingTheOysterBenchmarkTest.scala
+++ src/test/scala/part1/column1/CrackingTheOysterBenchmarkTest.scala
@@ -10,10 +10,21 @@ class CrackingTheOysterBenchmarkTest extends Bench.LocalTime {
   private val sortingAlgoGen = Gen.enumeration("algorithm")(MergeSort, ScalaDefaultSort, ScalaMergeSort)
   private val inputs = Gen.crossProduct(fileSizeGen, sortingAlgoGen)
 
+  private val externalSortingAlgoGen = Gen.enumeration("externalAlgorithm")(ExternalMergeSort)
+
   performance of "CrackingTheOyster" in {
     measure method "sortFile" in {
       using(inputs) in { case (size, algorithm) =>
         CrackingTheOyster.sortFile(s"src/test/scala/resources/part1/column1/listSize$size.txt", algorithm)
+      }
+    }
+
+    measure method "externalSortFile" in {
+      using(externalSortingAlgoGen) in { algorithm =>
+        CrackingTheOyster.externalSortFile(
+          "src/test/scala/resources/part1/column1/listSize1000000.txt",
+          "src/test/scala/resources/part1/column1/resultTmp.txt",
+          algorithm)
       }
     }
   }
